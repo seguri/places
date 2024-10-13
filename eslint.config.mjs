0505100ad7d31ad4ed436c1b8edbb00f30ff48f3
Marco Seguri <seguri@users.noreply.github.com>
2024-10-13 11:48:09 +0200

    Install eslint plugins
    With `npm i -D eslint-plugin-react@7.37.1 eslint-plugin-import@2.31.0 eslint-plugin-jsx-a11y@6.10.0`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,23 @@
 import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
+import a11yPlugin from "eslint-plugin-jsx-a11y";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import("eslint").Linter.Config[]} */
 export default [
   js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  a11yPlugin.flatConfigs.recommended,
   {
-    files: ["**/*.js", "**/*.jsx"],
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx,mts}"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
+      ecmaVersion: "latest",
+      sourceType: "module",
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
