fc1545c4bc61762bd7b5594817f4c510271db256
Marco Seguri <seguri@users.noreply.github.com>
2024-10-19 00:46:34 +0200

    Fix regression

--- src/Place.jsx
+++ src/Place.jsx
@@ -2,11 +2,15 @@ import { Marker, Popup } from "react-leaflet";
 import PropTypes from "prop-types";
 
 export default function Place({ feature, icon }) {
-  const [lat, lon, name, url] = feature;
+  const [lat, lon, name, cid] = feature;
   return (
     <Marker position={[lat, lon]} icon={icon}>
       <Popup>
-        <a href={url} target="_blank" rel="noopener noreferrer">
+        <a
+          href={`http://maps.google.com/?cid=${cid}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {name}
         </a>
       </Popup>
